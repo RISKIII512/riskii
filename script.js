0f70e388e42573ab623abc2503248342c4a5c5b8
RISKIII512 <riszcom0@gmail.com>
2024-12-20 04:14:25 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript ini digunakan untuk menambahkan efek atau interaksi lebih lanjut jika diperlukan
+// Misalnya, Anda dapat menambahkan alert atau animasi ketika pengguna mengklik link.
+
+document.querySelectorAll('.link-card').forEach (link => {
+    link.addEventListener('click', () => {
+      alert('Anda akan diarahkan ke halaman yang terkait selain link ini berarti clone.!');
+    });
+  });
+  
